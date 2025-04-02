cb2f1bc0f0ec3dfada8bda09630cb0a80bda52db
azsx92 <azsx92@gmail.com>
2025-04-02 16:44:43 +0900

    feat: flush end

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,14 +15,15 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         // 엔티티 매니저는 데이터 변경시 트랜잭션을 시작해야 한다.
         tx.begin();
-
+        // flush란 영속성 컨텍스트의 변경 내용을 데이터베이스에 반영하는 것이다.
+        // 그리고 나서 transaction이 커밋이 되는 것이다.
         try {
             //영속
 
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZ");
-            // 여기까지 INSERT SQL을 데이터베이스에 보내지 않는다.
+            Member member = new Member(170L, "member200");
+            em.persist(member);
 
+            em.flush();
             System.out.println("-======================-");
             // 커밋하는 순간 데이터 베이스에 INSERT SQL을 보낸다.
             tx.commit();
