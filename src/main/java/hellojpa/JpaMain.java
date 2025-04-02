2d79ae84c7df9e7fb824859ad87dd8306463b16b
azsx92 <azsx92@gmail.com>
2025-04-02 16:17:16 +0900

    feat: 영속 엔티티 동일성 보장

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,9 +17,9 @@ public class JpaMain {
 
         try {
 
-            Member findMember = em.find(Member.class, 101L);
+            Member findMember1 = em.find(Member.class, 101L);
             Member findMember2 = em.find(Member.class, 101L);
-
+            System.out.println("result = "  + (findMember1 == findMember2));
 
             tx.commit();
         } catch (Exception e) {
