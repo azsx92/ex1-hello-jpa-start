eb934201d1f44d2aae8d5ef8266098d6d9f43102
azsx92 <azsx92@gmail.com>
2025-04-02 15:40:37 +0900

    feat: JPQL example

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,7 +2,10 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
+
 
     public static void main(String[] args) {
 
@@ -13,10 +16,17 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class , 1L);
-            findMember.setName("HelloJPA");
+           // Member findMember = em.find(Member.class , 1L);
+           List<Member> result = em.createQuery("select m from Member as m",Member.class)
+                   .setFirstResult(5)
+                   .setMaxResults(8)
+                   .getResultList();
 
-            tx.commit();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+                tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
