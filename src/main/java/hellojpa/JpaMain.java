ac6d0b67e10bebe5754c79206b11eb18932e68ff
azsx92 <azsx92@gmail.com>
2025-04-02 22:10:13 +0900

    feat: generatedValue MEMBER_SEQ

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,10 +20,21 @@ public class JpaMain {
         try {
             //영속
             Member member = new Member();
-            member.setUsername("C");
+            member.setUsername("A");
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+           // em.persist(member);
+           // em.persist(member2);
+           // em.persist(member3);
+
             System.out.println("member.id :" + member.getId());
+            System.out.println("member.id :" + member2.getId());
+            System.out.println("member.id :" + member3.getId());
             System.out.println("-======================-");
             // 커밋하는 순간 데이터 베이스에 INSERT SQL을 보낸다.
             tx.commit();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -6,13 +6,16 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
-
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName="MEMBER_SEQ",
+        initialValue = 1 , allocationSize = 50)
 public class Member {
-
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
+    private Long id;
 
     @Column(name = "name", nullable = false )
     private String username;
