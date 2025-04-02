c912718e8ecd968b3b7a993f1c65d8784fe91efc
azsx92 <azsx92@gmail.com>
2025-04-02 16:14:53 +0900

    feat: 1차 캐시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,19 +16,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-                // 객체를 생성한 상태(비영속)
-                Member member = new Member();
-                member.setId(101L);
-                member.setName("회원1");
-
-            System.out.println("=== START ===");
-            em.persist(member);
-            System.out.println("=== END ===");
 
             Member findMember = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
 
-            System.out.println("findMember.id :" + findMember.getId());
-            System.out.println("findMember.name :" + findMember.getName());
 
             tx.commit();
         } catch (Exception e) {
