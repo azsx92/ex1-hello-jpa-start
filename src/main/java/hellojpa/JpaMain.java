26f48b258c2c37000ed96fc38e6beda201478a30
azsx92 <azsx92@gmail.com>
2025-04-02 16:13:48 +0900

    feat: entityManger

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,17 +16,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-           // Member findMember = em.find(Member.class , 1L);
-           List<Member> result = em.createQuery("select m from Member as m",Member.class)
-                   .setFirstResult(5)
-                   .setMaxResults(8)
-                   .getResultList();
+                // 객체를 생성한 상태(비영속)
+                Member member = new Member();
+                member.setId(101L);
+                member.setName("회원1");
 
+            System.out.println("=== START ===");
+            em.persist(member);
+            System.out.println("=== END ===");
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
-                tx.commit();
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id :" + findMember.getId());
+            System.out.println("findMember.name :" + findMember.getName());
+
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
