fe0b15951cf56711ef8da1bf003ec0fdf86dbb2f
azsx92 <azsx92@gmail.com>
2025-04-04 17:05:41 +0900

    feat: 1:N

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -15,7 +15,7 @@ public class Team {
 
     private String name;
     @OneToMany
-    @JoinColumn(name = "TEAM_ID")
+    //@JoinColumn(name = "TEAM_ID")
     private List<Member> members = new ArrayList<>();
 
     public List<Member> getMembers() {
